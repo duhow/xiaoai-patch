861ee23c884f545505e88d4119b5466d5c6e6527
duhow <contacto@duhowpi.net>
2020-12-23 21:16:25 +0100

    Update ffmpeg and remove repeated warnings

--- packages/ffmpeg/package.mk
+++ packages/ffmpeg/package.mk
@@ -1,12 +1,23 @@
 PACKAGE_NAME="FFmpeg"
-PACKAGE_VERSION="4.3"
-PACKAGE_SRC="https://www.ffmpeg.org/releases/ffmpeg-4.3.tar.gz"
+PACKAGE_VERSION="4.3.1"
+PACKAGE_SRC="https://www.ffmpeg.org/releases/ffmpeg-${PACKAGE_VERSION}.tar.gz"
+PACKAGE_DEPENDS="openssl"
 
 configure_package() {
 	export PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}"
 	export PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}"
 	# compile with -fpic flag, otherwise the soxr package will fail to build
-	./configure --prefix=${INSTALL_PREFIX} --arch=${BUILD_TARGET} --target-os=linux --cross-prefix=${BUILD_TARGET}- --cc="${BUILD_CC} -fPIC" --cxx="${BUILD_CXX}" --extra-cflags="${BUILD_CFLAGS}" --extra-ldflags="${BUILD_LDFLAGS}" --pkg-config="/usr/bin/pkg-config" --enable-openssl 
+	./configure \
+		--prefix=${INSTALL_PREFIX} \
+		--arch=${BUILD_TARGET} \
+		--target-os=linux \
+		--cross-prefix=${BUILD_TARGET}- \
+		--cc="${BUILD_CC} -fPIC" \
+		--cxx="${BUILD_CXX}" \
+		--extra-cflags="${BUILD_CFLAGS} -w" \
+		--extra-ldflags="${BUILD_LDFLAGS}" \
+		--pkg-config="/usr/bin/pkg-config" \
+		--enable-openssl
 }
 
 make_package() {
