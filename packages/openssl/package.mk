5df19ebee2226e5b6495bd5a7c034745ab30511e
duhow <contacto@duhowpi.net>
2020-12-23 21:20:12 +0100

    Remove openssl documentation build

--- packages/openssl/package.mk
+++ packages/openssl/package.mk
@@ -1,6 +1,6 @@
 PACKAGE_NAME="OpenSSL"
 PACKAGE_VERSION="1.1.1h"
-PACKAGE_SRC="https://www.openssl.org/source/openssl-1.1.1h.tar.gz"
+PACKAGE_SRC="https://www.openssl.org/source/openssl-${PACKAGE_VERSION}.tar.gz"
 
 configure_package() {
 	CFLAGS="${BUILD_CFLAGS}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" ./Configure linux-generic32 --prefix=${INSTALL_PREFIX} --cross-compile-prefix=${BUILD_TARGET}-
@@ -11,5 +11,5 @@ make_package() {
 }
 
 install_package() {
-	make DESTDIR=${STAGING_DIR} install
+	make DESTDIR=${STAGING_DIR} install_sw install_ssldirs
 }
