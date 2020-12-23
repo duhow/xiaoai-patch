a208274f2764dff1ec6696c0474e6a5516f28080
duhow <contacto@duhowpi.net>
2020-12-23 21:16:48 +0100

    Add LAME mp3 dependency

--- packages/lame/package.mk
+++ packages/lame/package.mk
@@ -1,6 +1,7 @@
 PACKAGE_NAME="LAME MP3 Encoder"
 PACKAGE_VERSION="3.100"
-PACKAGE_SRC="https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz"
+PACKAGE_SRC="https://sourceforge.net/projects/lame/files/lame/${PACKAGE_VERSION}/lame-${PACKAGE_VERSION}.tar.gz"
+PACKAGE_DEPENDS="ncurses"
 
 configure_package() {
 	CC="${BUILD_CC}" CXX="${BUILD_CXX}" CFLAGS="${BUILD_CFLAGS}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS} -ltinfo" PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET}
