f412437ebe81c5eb71b44a0eb631d275f1a1bd10
duhow <contacto@duhowpi.net>
2020-12-26 18:08:42 +0100

    Add libical package - took couple of days to get it done :(

--- packages/libical/package.mk
+++ packages/libical/package.mk
@@ -4,53 +4,25 @@ PACKAGE_SRC="https://github.com/libical/libical/releases/download/v${PACKAGE_VER
 PACKAGE_DEPENDS="glibc glib libxml"
 PACKAGE_USE_SEPARATE_BUILD_DIR="true"
 
-configure_package() {
-	#-DCMAKE_SYSROOT=${STAGING_DIR} \
-	# CXXFLAGS="-I${STAGING_DIR}/${INSTALL_PREFIX}/${BUILD_TARGET}/include ${BUILD_CFLAGS}" \
-	#-D_GLIBCXX_USE_C99=ON \
-	#-D_GLIBCXX_HAVE_WCSTOF=1 \
+preconfigure_package() {
+	in_to_out ${PACKAGE_DIR}/cmake-toolchain.txt.in ${PACKAGE_SRC_DIR}/cmake-toolchain.txt
+}
 
+configure_package() {
 	PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" \
 	PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
 	cmake \
 		-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-		-DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_CMAKE} \
+		-DCMAKE_TOOLCHAIN_FILE=${PACKAGE_SRC_DIR}/cmake-toolchain.txt \
 		-DENABLE_GTK_DOC=OFF \
 		-DICAL_BUILD_DOCS=OFF \
+		-DICAL_GLIB=False \
 		-DLIBICAL_BUILD_TESTING=OFF \
 		${PACKAGE_SRC_DIR}
 }
 
-premake_package() {
-	# FIXME this build is unstable, as it uses mixed libs from ARM and system.
-	# Some commands used to fix this:
-
-	echo_error "setting a horrible hack to fix the build"
-
-	FILE_XLOCALE=${TOOLCHAIN_DIR}/${BUILD_TARGET}/libc/usr/include/xlocale.h
-	FILE_LIBRESOLV=${TOOLCHAIN_DIR}/${BUILD_TARGET}/libc/lib/libresolv.so.2
-
-	mv ${FILE_XLOCALE} ${FILE_XLOCALE}.bak
-	ln -s ${STAGING_DIR}/usr/include/locale.h ${FILE_XLOCALE}
-
-	mv ${FILE_LIBRESOLV} ${FILE_LIBRESOLV}.bak
-	ln -s ${STAGING_DIR}/lib/libresolv.so.2 ${FILE_LIBRESOLV}
-}
-
-
 make_package() {
 	make -j${MAKE_JOBS}
-	EXITCODE=$?
-
-	echo_info "undoing toolchain changes"
-
-	rm ${FILE_XLOCALE}
-	mv ${FILE_XLOCALE}.bak ${FILE_XLOCALE}
-
-	rm ${FILE_LIBRESOLV}
-	mv ${FILE_LIBRESOLV}.bak ${FILE_LIBRESOLV}
-
-	return ${EXITCODE}
 }
 
 install_package() {
