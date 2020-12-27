561def71d0b82dede583c09c670bbd0a7f90a353
duhow <contacto@duhowpi.net>
2020-12-28 00:50:30 +0100

    Group all base packages to update

---
+++ packages/update-binaries/package.mk
@@ -0,0 +1,2 @@
+PACKAGE_NAME="Update all existing binaries in Xiaoai"
+PACKAGE_DEPENDS="busybox dropbear htop curl alsa blue-alsa"
---
+++ packages/update-libs/package.mk
@@ -0,0 +1,2 @@
+PACKAGE_NAME="Update all existing libs in Xiaoai"
+PACKAGE_DEPENDS="glibc zlib gcc openssl ncurses readline libogg opus pcre libsbc libical glib curl bluez expat libffi libcrypto dbus alsa-lib libxml"
