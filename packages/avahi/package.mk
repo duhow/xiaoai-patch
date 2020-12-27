d99b587fd6fc9cfd4755ff88270c9b1d09c780fe
duhow <contacto@duhowpi.net>
2020-12-27 11:01:11 +0100

    Fix avahi run

--- packages/avahi/package.mk
+++ packages/avahi/package.mk
@@ -7,7 +7,7 @@ configure_package() {
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --build=${MACHTYPE} --host=${BUILD_TARGET} \
+	   ./autogen.sh --build=${MACHTYPE} --host=${BUILD_TARGET} \
 	   --prefix=${INSTALL_PREFIX} --sysconfdir=/etc --localstatedir=/var \
 	   --with-distro=debian \
 	   --disable-qt3 --disable-qt4 --disable-qt5 \
@@ -22,4 +22,11 @@ make_package() {
 
 install_package() {
 	make DESTDIR=${STAGING_DIR} install
+}
+
+postinstall_package() {
+	echo_info "adding rc.d start for avahi"
+	RCD_FOLDER="${STAGING_DIR}/etc/rc.d"
+	mkdir -p ${RCD_FOLDER}
+	ln -s /etc/init.d/avahi-daemon ${RCD_FOLDER}/S90avahi
 }
