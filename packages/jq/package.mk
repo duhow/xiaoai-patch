d44bb0da047b6ecf67bcd7509dcdc94d8c09e11b
duhow <contacto@duhowpi.net>
2020-12-29 17:26:49 +0100

    Add jq and tmux packages - jq is not latest version due to glibc dependency missing (header file) - tmux won't work due to locale UTF-8 missing

---
+++ packages/jq/package.mk
@@ -0,0 +1,24 @@
+PACKAGE_NAME="jq"
+PACKAGE_VERSION="1.5"
+PACKAGE_SRC="https://github.com/stedolan/jq/releases/download/jq-${PACKAGE_VERSION}/jq-${PACKAGE_VERSION}.tar.gz"
+PACKAGE_DEPENDS="gcc"
+
+preconfigure_package() {
+	autoreconf -fi
+}
+
+configure_package() {
+	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
+	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
+	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
+	   --disable-valgrind --disable-static --disable-docs
+}
+
+make_package() {
+	make -j${MAKE_JOBS}
+}
+
+install_package() {
+	make DESTDIR=${STAGING_DIR} install
+}
