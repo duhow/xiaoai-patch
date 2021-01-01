91630af42227ae203e473caf8a3b3171f25848ac
duhow <contacto@duhowpi.net>
2021-01-01 18:14:36 +0100

    Add echo canceller - test

---
+++ packages/ec/package.mk
@@ -0,0 +1,22 @@
+PACKAGE_NAME="echo canceller"
+PACKAGE_VERSION="master"
+PACKAGE_SRC="https://github.com/voice-engine/ec/archive/${PACKAGE_VERSION}.tar.gz"
+PACKAGE_DEPENDS="alsa-lib speexdsp"
+
+make_package() {
+	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" \
+	   PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+	   PKG_CONFIG_PATH="${BUILD_PKG_CONFIG_LIBDIR}" \
+	   \
+	   make -j${MAKE_JOBS} \
+	   \
+	   CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDLIBS="${BUILD_LDFLAGS} -ldl -lm -Wl,-Bstatic -Wl,-Bdynamic -lrt -lpthread -lasound -lspeexdsp" \
+	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" \
+	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" \
+	   PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+	   PKG_CONFIG_PATH="${BUILD_PKG_CONFIG_LIBDIR}"
+}
+
+install_package() {
+	cp -vf ec ec_hw ${STAGING_DIR}/usr/bin
+}
