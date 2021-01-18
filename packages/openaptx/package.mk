0ae71e8d5b7f6437624d985cb5c117880fcddcc7
duhow <contacto@duhowpi.net>
2021-01-18 17:34:03 +0100

    Update blue-alsa enable APTX

--- packages/openaptx/package.mk
+++ packages/openaptx/package.mk
@@ -1,22 +1,22 @@
 PACKAGE_NAME="openaptX"
-PACKAGE_VERSION="1.2.0"
+PACKAGE_VERSION="1.3.0"
 PACKAGE_SRC="https://github.com/Arkq/openaptx/archive/v${PACKAGE_VERSION}.tar.gz"
 PACKAGE_DEPENDS="libsndfile ffmpeg"
+PACKAGE_USE_SEPARATE_BUILD_DIR="true"
 
-preconfigure_package() {
-	autoreconf --install
+preconfigure_package(){
+	in_to_out ${PACKAGE_DIR}/cmake-toolchain.txt.in cmake-toolchain.txt
 }
 
 configure_package() {
-
-	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS} -lm" \
-	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
-	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
-	   --with-sysroot=${STAGING_DIR} \
-	   --with-ffmpeg --with-sndfile \
-	   --enable-aptx422 --enable-aptxHD100 \
-	   --disable-doc
+	PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" \
+	PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+	cmake \
+		-DCMAKE_TOOLCHAIN_FILE=cmake-toolchain.txt \
+		-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+		-DWITH_FFMPEG=ON -DWITH_SNDFILE=ON \
+		-DENABLE_APTX422=ON -DENABLE_APTXHD100=ON \
+		${PACKAGE_SRC_DIR}
 }
 
 make_package() {
