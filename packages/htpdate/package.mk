fd104e2ee23f67cb59a566671d34990961231736
duhow <contacto@duhowpi.net>
2021-05-29 20:15:08 +0200

    Add htpdate package

---
+++ packages/htpdate/package.mk
@@ -0,0 +1,15 @@
+PACKAGE_NAME="htpdate"
+PACKAGE_VERSION="1.2.6"
+PACKAGE_SRC="https://github.com/angeloc/htpdate/archive/refs/tags/v${PACKAGE_VERSION}.tar.gz"
+PACKAGE_DEPENDS="openssl"
+
+make_package() {
+	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
+	CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
+	PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+	make ENABLE_HTTPS=1
+}
+
+install_package() {
+	make DESTDIR=${STAGING_DIR} install
+}
