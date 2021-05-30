e8e952ba79743da6865c7a258e68005c76d0002f
duhow <contacto@duhowpi.net>
2021-05-30 23:40:14 +0200

    Fix some errors in packages

--- packages/bzip2/package.mk
+++ packages/bzip2/package.mk
@@ -1,6 +1,6 @@
 PACKAGE_NAME="bzip2"
 PACKAGE_VERSION="1.0.8"
-PACKAGE_SRC="https://www.sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz"
+PACKAGE_SRC="https://www.sourceware.org/pub/bzip2/bzip2-${PACKAGE_VERSION}.tar.gz"
 
 install_package() {
 	make CC="${BUILD_CC} -fPIC" AR="${BUILD_TARGET}-ar" RANLIB="${BUILD_TARGET}-ranlib" LDFLAGS="${BUILD_LDFLAGS}" libbz2.a bzip2 bzip2recover
@@ -12,4 +12,9 @@ install_package() {
 
 postinstall_package() {
 	in_to_out ${PACKAGE_DIR}/config/bzip2.pc.in ${STAGING_DIR}/${INSTALL_PREFIX}/lib/pkgconfig/bzip2.pc
+	# fix broken symlinks
+	ln -sf bzdiff ${STAGING_DIR}/${INSTALL_PREFIX}/bin/bzcmp
+	ln -sf bzgrep ${STAGING_DIR}/${INSTALL_PREFIX}/bin/bzegrep
+	ln -sf bzgrep ${STAGING_DIR}/${INSTALL_PREFIX}/bin/bzfgrep
+	ln -sf bzmore ${STAGING_DIR}/${INSTALL_PREFIX}/bin/bzless
 }
