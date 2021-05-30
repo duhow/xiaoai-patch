d3126bf2f58f716e77f81925875adb84aed64120
duhow <contacto@duhowpi.net>
2021-05-30 23:40:35 +0200

    Improve patches and add few more for LX01

--- scripts/20_disable_services.sh
+++ scripts/20_disable_services.sh
@@ -5,6 +5,7 @@ for SERVICE in \
   work_day_sync_service xiaomi_dns_server mediaplayer messagingagent \
   wifitool mitv-disc miio pns mibrain_service mico_ai_crontab mico_ir_agent \
   nano_httpd pns_ubus_helper quickplayer voip mdplay mibt_mesh_proxy \
+  didiagent mico_voip_ubus_service aw_upgrade_autorun \
   statpoints_daemon alarm notify dlnainit touchpad sound_effect linein; do
 
   rm -vf $ROOTFS/etc/rc.d/S??${SERVICE}
