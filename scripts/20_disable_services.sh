795f650b0b88d1281d898248cd5c8dc35a346fd1
duhow <contacto@duhowpi.net>
2021-07-05 18:44:42 +0200

    L09A: remove services startup

--- scripts/20_disable_services.sh
+++ scripts/20_disable_services.sh
@@ -6,6 +6,7 @@ for SERVICE in \
   wifitool mitv-disc miio pns mibrain_service mico_ai_crontab mico_ir_agent \
   nano_httpd pns_ubus_helper quickplayer voip mdplay mibt_mesh_proxy \
   didiagent mico_voip_ubus_service aw_upgrade_autorun \
+  mico_helper miot_agent mico_aivs_lab \
   statpoints_daemon alarm notify dlnainit touchpad sound_effect linein; do
 
   rm -vf $ROOTFS/etc/rc.d/S??${SERVICE}
