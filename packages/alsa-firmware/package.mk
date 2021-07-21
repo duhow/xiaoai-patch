121cf1f1aae68ce93a9bb6f8a99ad9b89401a601
hillbicks <git+hillbicks@schockel.net>
2021-07-21 13:43:34 +0200

    Update package.mk

--- packages/alsa-firmware/package.mk
+++ packages/alsa-firmware/package.mk
@@ -2,7 +2,6 @@ PACKAGE_NAME="Advanced Linux Sound Architecture - firmware"
 PACKAGE_VERSION="1.2.4"
 PACKAGE_SRC="https://github.com/alsa-project/alsa-firmware/archive/refs/tags/v${PACKAGE_VERSION}.tar.gz"
 
-
 configure_package() {
 	CC="${BUILD_CC}" CXX="${BUILD_CXX}" CFLAGS="${BUILD_CFLAGS}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET}
 }
