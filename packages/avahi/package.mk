4f40fb553c46420ef908435a624b2bf77d58e8f0
duhow <contacto@duhowpi.net>
2021-07-22 21:53:03 +0200

    Fix avahi url

--- packages/avahi/package.mk
+++ packages/avahi/package.mk
@@ -1,8 +1,7 @@
 PACKAGE_NAME="Avahi"
 PACKAGE_VERSION="0.8"
 PACKAGE_DEPENDS="libdaemon libevent glib dbus gdbm"
-PACKAGE_SRC="https://github.com/lathiat/avahi/releases/download/v0.8/avahi-${PACKAGE_VERSION}.tar.gz"
-
+PACKAGE_SRC="https://github.com/lathiat/avahi/releases/download/v${PACKAGE_VERSION}/avahi-${PACKAGE_VERSION}.tar.gz"
 
 configure_package() {
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
