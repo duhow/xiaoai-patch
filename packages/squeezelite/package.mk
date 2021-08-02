e2d86d720e5147de3beb5b113d9cd60a6a4fa72a
duhow <contacto@duhowpi.net>
2021-08-02 19:07:14 +0200

    Add init script

--- packages/squeezelite/package.mk
+++ packages/squeezelite/package.mk
@@ -17,4 +17,7 @@ preinstall_package() {
 
 install_package() {
 	install -v squeezelite ${STAGING_DIR}/usr/bin
+
+	install -v ${PACKAGE_DIR}/config/${PACKAGE_NAME}.init ${STAGING_DIR}/etc/init.d/${PACKAGE_NAME}
+	ln -svf ../init.d/${PACKAGE_NAME} ${STAGING_DIR}/etc/rc.d/S99${PACKAGE_NAME}
 }
