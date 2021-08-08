692ff9fbd2531eb533e63b0c4569e3481c74e1db
duhow <contacto@duhowpi.net>
2021-08-08 15:19:43 +0200

    Add model to build process

--- packages/update-libs/package.mk
+++ packages/update-libs/package.mk
@@ -1,2 +1,6 @@
 PACKAGE_NAME="Update all existing libs in Xiaoai"
-PACKAGE_DEPENDS="glibc zlib gcc openssl ncurses readline libogg opus pcre sbc libical glib curl bluez expat libffi dbus alsa-lib libxml"
+PACKAGE_DEPENDS="glibc zlib gcc openssl ncurses readline libogg opus pcre sbc libical glib curl expat libffi dbus alsa-lib libxml"
+
+if [ "${BUILD_MODEL}" != "LX01" ]; then
+PACKAGE_DEPENDS="${PACKAGE_DEPENDS} bluez"
+fi
