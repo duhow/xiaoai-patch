793ac5937e7306465049d01d07cc43d45677421d
duhow <contacto@duhowpi.net>
2021-08-08 20:07:27 +0200

    Revert and update SQLite package
    Official package bundles a manifest with SHA3 signatures, while
    Github mirror does not include it, and requires additional programs to
    build it.

--- packages/sqlite3/package.mk
+++ packages/sqlite3/package.mk
@@ -1,13 +1,18 @@
 PACKAGE_NAME="SQLite"
 PACKAGE_VERSION="3.36.0"
-PACKAGE_SRC="https://github.com/mackyle/sqlite/archive/refs/tags/version-${PACKAGE_VERSION}.tar.gz"
+PACKAGE_SRC="https://www.sqlite.org/2021/sqlite-autoconf-${PACKAGE_VERSION//./}000.tar.gz"
 PACKAGE_DEPENDS="zlib readline"
+
+preconfigure_package() {
+	autoreconf -fi
+}
 
 configure_package() {
 	CC="${BUILD_CC}" CFLAGS="-DSQLITE_ENABLE_UNLOCK_NOTIFY ${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET}
+	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
+	   --disable-tcl
 }
 
 make_package() {
