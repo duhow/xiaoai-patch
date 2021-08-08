5e67dfa14691e57e31b21304f618dca7a38fc0ef
duhow <contacto@duhowpi.net>
2021-08-08 12:22:05 +0200

    Update jq

--- packages/jq/package.mk
+++ packages/jq/package.mk
@@ -1,5 +1,5 @@
 PACKAGE_NAME="jq"
-PACKAGE_VERSION="1.5"
+PACKAGE_VERSION="1.6"
 PACKAGE_SRC="https://github.com/stedolan/jq/releases/download/jq-${PACKAGE_VERSION}/jq-${PACKAGE_VERSION}.tar.gz"
 PACKAGE_DEPENDS="gcc"
 
@@ -12,7 +12,8 @@ configure_package() {
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
 	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
-	   --disable-valgrind --disable-static --disable-docs
+	   --includedir="${STAGING_DIR}/${INSTALL_PREFIX}/include" \
+	   --disable-valgrind --enable-shared --disable-static --disable-docs --with-oniguruma=builtin
 }
 
 make_package() {
