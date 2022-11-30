8a97ac304f2ceeb2abdb7750bf6f0d588b3903e9
duhow <contacto@duhowpi.net>
2022-11-30 17:46:05 +0100

    Add bash symlink for usr built binary

--- scripts/95_add_shell_bash.sh
+++ scripts/95_add_shell_bash.sh
@@ -2,6 +2,11 @@
 
 FILE=/bin/bash
 
+if [ -f "${ROOTFS}/usr${FILE}" ] && [ ! -e "${ROOTFS}${FILE}" ]; then
+  echo "[*] Adding symlink for bash"
+  ln -svf /usr${FILE} ${ROOTFS}${FILE}
+fi
+
 if [ -f "${ROOTFS}${FILE}" ]; then
   echo "[*] Enabling ${FILE} as shell"
   echo "${FILE}" >> $ROOTFS/etc/shells
