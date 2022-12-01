882fccb7b05a36380d79af1f53eed95dfac6f9f7
duhow <contacto@duhowpi.net>
2022-12-01 14:27:04 +0100

    EXPERIMENTAL: symlink old libc libraries to save space
    Tested successfully with LX06

---
+++ scripts/96_compact_libs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LIBC_VERSIONS=$(find $ROOTFS/lib -name 'libc-*.so' -type f -exec basename {} \; | cut -d '-' -f2 | sort -n -r)
+LIBC_NEW=$(echo $LIBC_VERSIONS | awk '{print $1}')
+LIBC_OLD=$(echo $LIBC_VERSIONS | awk '{print $2}')
+
+if [ "$LIBC_NEW" = "$LIBC_OLD" ]; then
+  echo "[*] Found unique libc version $LIBC_NEW, skipping."
+  exit
+fi
+
+echo "[!] Replacing libc version $LIBC_OLD by $LIBC_NEW"
+for FILE in $ROOTFS/lib/*${LIBC_OLD}; do
+  rm -f $FILE
+  FILENAME=$(basename $FILE)
+  NEW_FILENAME=$(echo $FILENAME | sed "s/${LIBC_OLD}/${LIBC_NEW}/")
+  ln -svf $NEW_FILENAME $ROOTFS/lib/$FILENAME
+done
