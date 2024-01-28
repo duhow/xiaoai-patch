87287b51670d571dbd6a1a13953f565fd51760a3
duhow <contacto@duhowpi.net>
2024-01-28 18:03:08 +0100

    update python3 package

--- packages/python3/package.mk
+++ packages/python3/package.mk
@@ -4,69 +4,67 @@ PACKAGE_SRC="https://www.python.org/ftp/python/${PACKAGE_VERSION}/Python-${PACKA
 PACKAGE_DEPENDS="glibc ncurses"
 
 preconfigure_package() {
-        # build on self host
-        mkdir -p build-host
-        cp -a * build-host/
-        cd build-host
+	# build on self host
+	mkdir -p build-host
+	cp -a * build-host/
+	cd build-host
 
-        ./configure
-        make -j${MAKE_JOBS} python Parser/pgen
-        make install
+	./configure
+	make -j${MAKE_JOBS} python Parser/pgen
+	make install
 
-        if [ $? -gt 0 ]; then
-                return 1
-        fi
+	if [ $? -gt 0 ]; then
+		return 1
+	fi
 
-        cd ..
+	cd ..
 }
 
 configure_package() {
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
-	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
-	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --build=${MACHTYPE} --host=${BUILD_TARGET} --target=${BUILD_TARGET} \
-	   --prefix=${INSTALL_PREFIX} \
-	   ac_cv_file__dev_ptmx=no \
-	   ac_cv_file__dev_ptc=no \
-	   ac_cv_have_long_long_format=yes \
-	   --enable-optimizations \
-	   --with-lto \
-	   --enable-shared \
-           --disable-ipv6 \
-           --disable-test-modules \
-           --without-doc-strings \
-           --with-ensurepip=install
+	CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
+	PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+	./configure --build=${MACHTYPE} --host=${BUILD_TARGET} --target=${BUILD_TARGET} \
+	--prefix=${INSTALL_PREFIX} \
+	ac_cv_file__dev_ptmx=no \
+	ac_cv_file__dev_ptc=no \
+	ac_cv_have_long_long_format=yes \
+	--enable-optimizations \
+	--with-lto \
+	--enable-shared \
+	--disable-ipv6 \
+	--disable-test-modules \
+	--without-doc-strings \
+	--with-ensurepip=install
 }
 
 make_package() {
-        make -j${MAKE_JOBS} \
-                HOSTPYTHON=${PACKAGE_SRC_DIR}/build-host/python \
-                HOSTPGEN=${PACKAGE_SRC_DIR}/build-host/Parser/pgen \
-                CROSS_COMPILE_TARGET=yes \
-                LDFLAGS="${BUILD_LDFLAGS}" LINKFORSHARED=" "
+	make -j${MAKE_JOBS} \
+		HOSTPYTHON=${PACKAGE_SRC_DIR}/build-host/python \
+		HOSTPGEN=${PACKAGE_SRC_DIR}/build-host/Parser/pgen \
+		CROSS_COMPILE_TARGET=yes \
+		LDFLAGS="${BUILD_LDFLAGS}" LINKFORSHARED=" "
 }
 
 install_package() {
 	make DESTDIR=${STAGING_DIR} install
+}
 
-	# optimize size
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/pydoc_data
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/ensurepip
+postinstall_package() {
+	PYTHONDIR=`find ${STAGING_DIR}/usr/lib -maxdepth 1 -type d -name 'python3.*' -print -quit`
+	echo_notice "Python dir: ${PYTHONDIR}"
 
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/asyncio/windows_*.py
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/_osx_support.py
+	# cleanup of unused data, optimize size
+	for NAME in pydoc_data ensurepip 'asyncio/windows_*.py' _osx_support.py test unitest __pycache__ ; do
+		rm -rf ${PYTHONDIR}/${NAME}
+	done
 
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/test
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/test
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/*/test
+	find "${PYTHONDIR}" -type d -name '__pycache__' -exec rm -rf {} \;
 
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/unittest
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/idle_test
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/tests
+	for NAME in test idle_test tests ; do
+		find "${PYTHONDIR}" -mindepth 2 -type d -name "${NAME}" -exec rm -rf {} \;
+	done
 
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/__pycache__
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/__pycache__
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/*/__pycache__
-	rm -rvf /xiaoai/build-packages/staging/armv7/usr/lib/python3.*/*/*/*/__pycache__
-
+	# ignore if failed
+	return 0
 }
