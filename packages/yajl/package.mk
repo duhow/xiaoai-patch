cca5f9042059160f614465b32a9f6e39f28a2333
duhow <contacto@duhowpi.net>
2024-12-03 23:11:43 +0100

    package(yajl): update broken source
    Apparently there's a recent GitHub behaviour change that broke this
    download, where both a "branch" and a "tag" exist with same name, and it
    is not compatible (anymore).

--- packages/yajl/package.mk
+++ packages/yajl/package.mk
@@ -1,6 +1,6 @@
 PACKAGE_NAME="Yet Another JSON Library"
 PACKAGE_VERSION="2.1.0"
-PACKAGE_SRC="https://github.com/lloyd/yajl/archive/refs/tags/${PACKAGE_VERSION}.tar.gz"
+PACKAGE_SRC="https://github.com/lloyd/yajl/archive/refs/heads/${PACKAGE_VERSION}.tar.gz"
 
 configure_package() {
 	CC="${BUILD_CC}" CXX="${BUILD_CXX}" CFLAGS="${BUILD_CFLAGS}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" PKG_CONFIG_LIBDIR="${MPD_BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" ./configure -p ${INSTALL_PREFIX}
