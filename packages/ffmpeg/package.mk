45b2471a692f0d97c8d811b115e6c81ba710755d
duhow <contacto@duhowpi.net>
2024-12-10 22:24:43 +0100

    add remove config for ffmpeg

--- packages/ffmpeg/package.mk
+++ packages/ffmpeg/package.mk
@@ -7,6 +7,39 @@ configure_package() {
 	export PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}"
 	export PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}"
 	# compile with -fpic flag, otherwise the soxr package will fail to build
+	ENCODERS=""
+	for COD in aac ac3 alac aptx aptx_hd flac g723_1 opus vorbis zlib \
+		pcm_alaw pcm_mulaw pcm_s16be pcm_s16le pcm_s24be pcm_s24le pcm_u16be pcm_u16le pcm_u24be pcm_u24le ; do
+		ENCODERS="${ENCODERS} --enable-encoder=${COD}"
+	done
+
+	DIS_DECODERS=""
+	for COD in h261 h263 h263_v4l2m2m h263p h264_v4l2m2m h264 hevc \
+		mpeg1video mpeg2video mpeg4 mpeg4_v4l2m2m msmpeg4v2 msmpeg4v3 ; do
+		DIS_DECODERS="${DIS_DECODERS} --disable-decoder=${COD}"
+	done
+
+	VID_FILTERS=""
+	for FIL in addroi alphaextract alphamerge amplify ass atadenoise avgblur bbox bilateral \
+		bitplanenoise blackdetect blackframe blend bm3d boxblur bwdif cas chromahold \
+		chromakey chromanr chromashift ciescope colorbalance colorcontrast colorcorrect colorchannelmixer \
+		colorize colorkey colorhold colorlevels colormatrix colorspace convolution convolve \
+		coreimage crop cropdetect cue curves datascope dblur deband deblock decimate dedot deflicker \
+		derain deshake dilation displace drawbox exposure find_rect freezedetect freezeframes gblur gradfun \
+		histeq histogram hue interlace lagfun lenscorrection lensfun lumakey \
+		maskfun median monochrome msad ocr ocv overlay palettegen phase pixscope \
+		pseudocolor rotate scale scroll sr swapuv telecine transpose vflip vignette xbr xfade \
+		yadif zoompan zscale ; do
+		VID_FILTERS="${VID_FILTERS} --disable-filter=${FIL}"
+	done
+
+	DIS_OPTS=""
+	for OPT in libx264 libx265 crystalhd dxva2 cuda cuvid nvenc avisynth frei0r \
+		libopencore-amrnb libopencore-amrwb libdc1394 libgsm libilbc libvo-amrwbenc \
+		symver swscale ; do
+		DIS_OPTS="${DIS_OPTS} --disable-${OPT}"
+	done
+
 	./configure \
 		--prefix=${INSTALL_PREFIX} \
 		--arch=${BUILD_TARGET} \
@@ -20,13 +53,22 @@ configure_package() {
 		--enable-openssl \
 		--enable-shared --disable-static \
 		--enable-small \
+		--disable-runtime-cpudetect \
 		--disable-doc --disable-htmlpages --disable-manpages \
 		--enable-gpl --enable-nonfree \
+		${ENCODERS} \
+		${DIS_DECODERS} \
+		${VID_FILTERS} \
+		--enable-libfdk-aac \
 		--enable-libmp3lame \
 		--enable-libvorbis \
 		--enable-libspeex \
 		--enable-libopus \
-		--enable-libsoxr
+		--enable-libsoxr \
+		${DIS_OPTS}
+
+		# --disable-avfilter # REQUIRED by MPD
+
 }
 
 make_package() {
