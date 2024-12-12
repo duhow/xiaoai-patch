a221da1125a9a5ca870a839e350d33206cc523b2
duhow <contacto@duhowpi.net>
2024-12-13 00:32:29 +0100

    feat: install core_api in image
    Currently tested only on LX06. Still WIP but functional.

--- packages.sh
+++ packages.sh
@@ -698,7 +698,7 @@ mkdir -p ${STAGING_TO_TARGET_DIR}
 write_build_config
 [[ ! $? -eq 0 ]] && exit 1
 
-PACKAGES_TO_BUILD="update-libs update-binaries support jq services music python3 aec-cmdline screen improv getevent"
+PACKAGES_TO_BUILD="update-libs update-binaries support jq services music python3 aec-cmdline screen improv getevent core_api"
 
 for PKGN in $PACKAGES_TO_BUILD; do 
   process_package $PKGN
