fde31d88e2a9e787857f7253b4f6db5b5b75e2e4
duhow <contacto@duhowpi.net>
2024-12-19 12:37:35 +0100

    packages(squeezelite): do not build mad, use mpg123

--- packages/squeezelite/package.mk
+++ packages/squeezelite/package.mk
@@ -1,13 +1,13 @@
 PACKAGE_NAME="squeezelite"
 PACKAGE_VERSION="master"
 PACKAGE_SRC="https://github.com/ralph-irving/squeezelite/archive/refs/heads/master.tar.gz"
-PACKAGE_DEPENDS="alsa-lib openssl alac soxr faad2 opus opusfile libmad ffmpeg"
+PACKAGE_DEPENDS="alsa-lib openssl alac soxr faad2 opus opusfile mpg123 ffmpeg"
 
 make_package() {
 	make -j${MAKE_JOBS} \
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
-	   OPTS="-DFFMPEG -DALAC -DOPUS -DRESAMPLE -DLINKALL -DVISEXPORT -DDSD -DUSE_SSL -marm -I${STAGING_DIR}${INSTALL_PREFIX}/include/opus -I${STAGING_DIR}${INSTALL_PREFIX}/include/alac"
+	   OPTS="-DFFMPEG -DNO_MAD -DALAC -DOPUS -DRESAMPLE -DLINKALL -DVISEXPORT -DDSD -DUSE_SSL -marm -I${STAGING_DIR}${INSTALL_PREFIX}/include/opus -I${STAGING_DIR}${INSTALL_PREFIX}/include/alac"
 }
 
 preinstall_package() {
