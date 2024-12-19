3f351a81a2765b39dc21bd1398f87e1604903f00
duhow <contacto@duhowpi.net>
2024-12-19 12:38:38 +0100

    packages: include squeezelite

--- packages/music/package.mk
+++ packages/music/package.mk
@@ -1,2 +1,2 @@
 PACKAGE_NAME="Packages for music playing"
-PACKAGE_DEPENDS="mpg123 vorbis-tools opus-tools mpd mpc ffmpeg upmpdcli shairport-sync-metadata snapcast"
+PACKAGE_DEPENDS="mpg123 vorbis-tools opus-tools mpd mpc ffmpeg upmpdcli shairport-sync-metadata snapcast squeezelite"
