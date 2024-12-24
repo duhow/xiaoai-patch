b9d3e8d4699433acfa7256591610751c5cb4ced9
duhow <contacto@duhowpi.net>
2024-12-24 14:31:44 +0100

    LX01: fix glibc not found

--- scripts/96_compact_libs.sh
+++ scripts/96_compact_libs.sh
@@ -9,6 +9,29 @@ if [ "$LIBC_NEW" = "$LIBC_OLD" ]; then
   exit
 fi
 
+if [ "${MODEL}" = "lx01" ] ; then
+  echo "[!] Fixing libs for ${MODEL}"
+  # NOTE: this cannot be done in build package process
+  # as generates issues with gcc builds of multiple programs (glibc -> gcc, openssl)
+
+  FILE="libgcc_s.so"
+  LIB_TARGET="$ROOTFS/usr/lib/${FILE}"
+  rm -vf ${LIB_TARGET}
+  ln -svf ${FILE}.1 ${LIB_TARGET}
+
+  # this file is old, the new one is in /lib
+  FILE="libuuid.so.1.3.0"
+  LIB_TARGET="$ROOTFS/usr/lib/${FILE}"
+  rm -vf ${LIB_TARGET}
+  ln -svf ../../lib/${FILE} ${LIB_TARGET}
+
+  for FILE in libc libpthread ; do
+    LIB_TARGET="$ROOTFS/usr/lib/${FILE}.so"
+    rm -vf ${LIB_TARGET}
+    ln -svf ../../lib/${FILE}-${LIBC_NEW}.so ${LIB_TARGET}
+  done
+fi
+
 if [ -z "$LIBC_OLD" ]; then
   echo "[*] libc not found (musl?), skipping."
   exit
