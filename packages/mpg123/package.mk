duhow/xiaoai-patch

commit c92e54b6d26c6e11aeb0c6acded979bde04ec455

packages: disable build of (more) unused bin tools

packages/mpg123/package.mk
@@ -7,7 +7,10 @@ configure_package() {
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET}
+	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
+		 --disable-programs \
+		 --disable-components \
+		 --enable-libmpg123
 }
 
 make_package() {
@@ -16,4 +19,10 @@ make_package() {
 
 install_package() {
 	make DESTDIR=${STAGING_DIR} install
+}
+
+postinstall_package() {
+  for FILE in mpg123-id3dump mpg123-strip ; do
+    rm -vf ${STAGING_DIR}/usr/bin/${FILE}
+  done
 }

packages/lame/package.mk
@@ -4,7 +4,11 @@ PACKAGE_SRC="https://sourceforge.net/projects/lame/files/lame/${PACKAGE_VERSION}
 PACKAGE_DEPENDS="ncurses"
 
 configure_package() {
-	CC="${BUILD_CC}" CXX="${BUILD_CXX}" CFLAGS="${BUILD_CFLAGS}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS} -ltinfo" PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET}
+	CC="${BUILD_CC}" CXX="${BUILD_CXX}" CFLAGS="${BUILD_CFLAGS}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
+		 LDFLAGS="${BUILD_LDFLAGS} -ltinfo" \
+		 PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
+		 ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
+		 --disable-frontend
 }
 
 make_package() {

packages/sbc/package.mk
@@ -12,7 +12,9 @@ configure_package() {
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} --enable-high-precision
+	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
+		 --enable-high-precision \
+		 --disable-static --disable-tools --disable-tester
 }
 
 make_package() {

packages/speex/package.mk
@@ -10,7 +10,8 @@ configure_package() {
 	CC="${BUILD_CC}" CFLAGS="${BUILD_CFLAGS}" LDFLAGS="${BUILD_LDFLAGS}" \
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
-	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET}
+	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
+		 --disable-binaries
 }
 
 make_package() {

commit 222c74c7a07256f5924da806a459cc4285751263

fix: add mpg123 binary and required libs
caused by c92e54b

packages/mpg123/package.mk
@@ -8,9 +8,12 @@ configure_package() {
 	   CXX="${BUILD_CXX}" CXXFLAGS="${BUILD_CFLAGS}" CPPFLAGS="${BUILD_CFLAGS}" \
 	   PKG_CONFIG_LIBDIR="${BUILD_PKG_CONFIG_LIBDIR}" PKG_CONFIG_SYSROOT_DIR="${BUILD_PKG_CONFIG_SYSROOT_DIR}" \
 	   ./configure --prefix=${INSTALL_PREFIX} --build=${MACHTYPE} --host=${BUILD_TARGET} \
-		 --disable-programs \
 		 --disable-components \
-		 --enable-libmpg123
+		 --enable-programs \
+		 --enable-libmpg123 \
+		 --enable-libout123 \
+		 --enable-libout123-modules \
+		 --with-audio=alsa
 }
 
 make_package() {
@@ -22,7 +25,7 @@ install_package() {
 }
 
 postinstall_package() {
-  for FILE in mpg123-id3dump mpg123-strip ; do
+  for FILE in out123 mpg123-id3dump mpg123-strip ; do
     rm -vf ${STAGING_DIR}/usr/bin/${FILE}
   done
 }